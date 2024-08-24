90efb6e603748be14e51b027d10d619729562c33
Pulkit Guglani <pulkitname@gmail.com>
2024-08-25 01:04:26 +0530

    test: added new test case for custom delimiter ( step 4 )

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -6,4 +6,6 @@ describe("adding multiple numbers", () => {
   test("sum of multiple numbers", () => expect(sum("1,2,3,4,5,6")).toBe(21));
   test("sum of multiple numbers separated by \n", () =>
     expect(sum("1,\n2,3")).toBe(6));
+  test("sum with numbers separated by custom delimiter", () =>
+    expect(sum("//;\n1;2")).toBe(3));
 });
