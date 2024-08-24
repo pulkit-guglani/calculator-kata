e3f442e51f9b50262ccec61b503c07c53f6d946c
Pulkit Guglani <pulkitname@gmail.com>
2024-08-25 00:15:02 +0530

    test: added test case for empty string and multiple numbers (step 2)

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,4 +2,6 @@ import sum from "./calculator";
 
 describe("adding multiple numbers", () => {
   test("sum of 2 numbers", () => expect(sum("1,2")).toBe(3));
+  test("sum of empty string", () => expect(sum("")).toBe(0));
+  test("sum of multiple numbers", () => expect(sum("1,2,3,4,5,6")).toBe(21));
 });
