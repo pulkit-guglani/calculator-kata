0bbbebbcd1d90d6fb9a0aca4cf4e3fd1a0cb9f66
Pulkit Guglani <pulkitname@gmail.com>
2024-08-24 23:18:37 +0530

    Feature: implemented basic sum functionality to pass test case 1
    Implemented function to return sum of 2 numbers from string.
    Only implemented the logic to pass test case "sum of 2 numbers".

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,17 @@
-export const sum = (numbers) => {
-  return 0;
+const sum = (numbers) => {
+  if (!numbers) {
+    return 0;
+  }
+  const numbersList = numbers.split(",");
+  if (numbersList?.length === 0) {
+    return 0;
+  }
+  if (numbersList?.length === 1) {
+    return numbersList[0];
+  }
+  let sum = Number(numbersList?.[0]) + Number(numbersList?.[1]) ?? 0;
+
+  return sum;
 };
+
+export default sum;
