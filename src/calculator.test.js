ded15bc0e17f01b9b8f52968a50df40280b79cbb
Pulkit Guglani <pulkitname@gmail.com>
2024-08-25 00:49:01 +0530

    test: added new test case for \n as a separator ( step 3)

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,4 +4,6 @@ describe("adding multiple numbers", () => {
   test("sum of 2 numbers", () => expect(sum("1,2")).toBe(3));
   test("sum of empty string", () => expect(sum("")).toBe(0));
   test("sum of multiple numbers", () => expect(sum("1,2,3,4,5,6")).toBe(21));
+  test("sum of multiple numbers separated by \n", () =>
+    expect(sum("1,\n2,3")).toBe(6));
 });
