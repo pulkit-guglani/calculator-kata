842fd2bebd7c3aac97891918a1e082b849087412
Pulkit Guglani <pulkitname@gmail.com>
2024-08-25 15:57:05 +0530

    Feature: added functionality to add custom delimiter
    to pass the test case of step 4 for custom delimiter, modified the sum function accordingly

--- src/calculator.js
+++ src/calculator.js
@@ -2,10 +2,26 @@ const sum = (numbers) => {
   if (!numbers) {
     return 0;
   }
-  const numbersList = numbers.split(/[\n,]/);
+
+  const delimiterRegex = /^\/\/(.+)\n([\s\S]*)$/;
+  const match = numbers.match(delimiterRegex);
+
+  let delimiter = ","; // default delimiter
+  let numbersString = numbers;
+
+  if (match) {
+    delimiter = match[1];
+    numbersString = match[2];
+  } else {
+    numbersString = numbers;
+  }
+  const splitRegex = new RegExp(`[${delimiter}\n]`);
+  const numbersList = numbersString.split(splitRegex);
+
   if (numbersList?.length === 0) {
     return 0;
   }
+
   let sum = 0;
   numbersList.forEach((number) => {
     if (number) {
